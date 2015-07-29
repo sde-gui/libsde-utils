451ac0770e789d91e87d7911aa41049a172aab26
Vadim Ushakov <igeekless@gmail.com>
2015-07-29 21:04:43 +0700

    make autogen.sh work on OpenBSD

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,16 @@ AUTOPOINT="${AUTOPOINT:-autopoint}"
 INTLTOOLIZE="${INTLTOOLIZE:-intltoolize}"
 AUTORECONF="${AUTORECONF:-autoreconf}"
 
+if [[ "`uname`" == OpenBSD ]] ; then
+    V="`ls -1 /usr/local/bin/autoreconf-* | env LC_ALL=C sort | tail -n 1`"
+    V="${V##*-}"
+    export AUTOCONF_VERSION="${AUTOCONF_VERSION:-$V}"
+
+    V="`ls -1 /usr/local/bin/automake-* | env LC_ALL=C sort | tail -n 1`"
+    V="${V##*-}"
+    export AUTOMAKE_VERSION="${AUTOMAKE_VERSION:-$V}"
+fi
+
 #$AUTOPOINT --force || exit $?
 #export AUTOPOINT="$INTLTOOLIZE --automake --copy"
 $AUTORECONF --force --install --verbose
